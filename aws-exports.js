2f7a4fb7a216cb52ed026be9fc12cf0de41092bc
Saksham kala <www.sakshamkala222@gmail.com>
2025-09-30 11:04:04 +0530

    Update aws-exports.js

--- aws-exports.js
+++ aws-exports.js
@@ -3,25 +3,26 @@
 const awsExports = {
   Auth: {
     Cognito: {
-      // Replace these with the values from your Cognito setup
-      userPoolId: "eu-north-1_0rMffvOo6",       // e.g., ap-south-1_AbCdEf123
-      userPoolClientId: "3ktmjs04suupvi40f1h8le0or3",   // e.g., 12ab34cd56efgh789ijklmn
-      signUpVerificationMethod: "code",        // or "link"
+      
+      userPoolId: "ur userpoolid",       
+      userPoolClientId: "poolclientid",   
+      signUpVerificationMethod: "code",      
       loginWith: {
-        // ✅ Amplify v6 requires you to specify allowed login methods
+        
         email: true,
         username: false,
         phone: false,
       },
       oauth: {
-        domain: "https://eu-north-10rmffvoo6.auth.eu-north-1.amazoncognito.com", // ✅ correct domain
+        domain: "domain", // ✅ correct domain
         scope: ["openid", "email", "phone"],
-        redirectSignIn: "http://localhost:3000/dashboard",
-        redirectSignOut: "http://localhost:3000/login",
+        redirectSignIn: "link",
+        redirectSignOut: "link",
         responseType: "code",
         }
     },
   },
 };
+
 
 export default awsExports;
